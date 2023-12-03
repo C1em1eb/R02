f14b94847890adad659f4edb789938dad8d6e49a
Clement Leblond <cleblond@c1r8s1.42prague.com>
2023-12-03 22:26:02 +0100

    new

--- libs/ft_get_dict.h
+++ libs/ft_get_dict.h
@@ -6,63 +6,62 @@
 /*   By: cleblond <cleblond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 13:23:35 by chanus            #+#    #+#             */
-/*   Updated: 2023/12/03 21:42:07 by cleblond         ###   ########.fr       */
+/*   Updated: 2023/12/03 20:47:00 by cleblond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers.h"
 #include "ft_strcat.h"
 
-char	*g_path = "./data/";
+void	fill_dict(char ***dictionary, char *buff, int ret, int *i)
+{
+	while (i[0] < ret && i[2] < 100)
+	{
+		i[1] = 0;
+		while (buff[i[0]] != ':')
+			dictionary[i[2]][0][i[1]++] = buff[i[0]++];
+		dictionary[i[2]][0][i[1]] = '\0';
+		i[0]++;
+		i[1] = 0;
+		while (buff[i[0]] != '\n' && i[0] < ret)
+			dictionary[i[2]][1][i[1]++] = buff[i[0]++];
+		dictionary[i[2]][1][i[1]] = '\0';
+		i[2]++;
+		i[0]++;
+	}
+}
 
 void	ft_get_dict(char *dict_name, char ***dictionary)
 {
-	char	*dictionary_name;
 	char	buff[BUFF_SIZE + 1];
 	int		fd;
 	int		ret;
-	int		c;
-	int		row;
-	int		o;
+	int		i[3];
 
-	o = 0;
-	c = 0;
-	row = 0;
-	dictionary_name = ft_strcat(g_path, dict_name);
-	fd = open(dictionary_name, O_RDONLY);
+	i[0] = 0;
+	i[1] = 0;
+	i[2] = 0;
+	fd = open(dict_name, O_RDONLY);
 	if (fd == -1)
-	{
-		return ;
-	}
+		ft_putstr("Dict Error\n");
 	ret = read(fd, buff, BUFF_SIZE);
 	buff[ret] = '\0';
-	while (c < ret && row < 100)
+	fill_dict(dictionary, buff, ret, i);
+	if (close(fd) == -1)
+		return ;
+}
+
+/*while (c < ret && row < 100)
 	{
 		o = 0;
 		while (buff[c] != ':')
-		{
-			dictionary[row][0][o] = buff[c];
-			o++;
-			c++;
-		}
+			dictionary[row][0][o++] = buff[c++];
 		dictionary[row][0][o] = '\0';
 		c++;
 		o = 0;
 		while (buff[c] != '\n' && c < ret)
-		{
-			dictionary[row][1][o] = buff[c];
-			o++;
-			c++;
-		}
+			dictionary[row][1][o++] = buff[c++];
 		dictionary[row][1][o] = '\0';
 		row++;
 		c++;
-	}
-	if (close(fd) == -1)
-	{
-		free(dictionary_name);
-		return ;
-	}
-	free(dictionary_name);
-	return ;
-}
+}*/
