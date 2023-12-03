75c530a646345cb4db82ea690b3dfd462ad324ea
Clement Leblond <cleblond@c1r8s1.42prague.com>
2023-12-03 22:41:41 +0100

    new 23h42

--- libs/ft_get_charnum.h
+++ libs/ft_get_charnum.h
@@ -6,7 +6,7 @@
 /*   By: cleblond <cleblond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 19:12:48 by chanus            #+#    #+#             */
-/*   Updated: 2023/12/03 22:02:34 by cleblond         ###   ########.fr       */
+/*   Updated: 2023/12/03 22:38:46 by cleblond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,36 +30,12 @@ int	get_digits(int num)
 	return (i);
 }
 
-char	*ft_get_charnum(int nb)
+void	reverse(char *str_res, int i)
 {
-	char			*str_res;
-	int				i;
-	unsigned int	ui;
-	int				start;
-	int				end;
-	char			temp;
+	int		start;
+	int		end;
+	char	temp;
 
-	i = 0;
-	str_res = malloc(get_digits(nb) + 1);
-	if (nb < 0)
-	{
-		write(1, "-", 1);
-		ui = nb * -1;
-	}
-	else
-		ui = nb;
-	while (ui > 0)
-	{
-		str_res[i++] = '0' + (ui % 10);
-		ui /= 10;
-	}
-	if (nb != 0)
-		str_res[i] = '\0';
-	else
-	{
-		str_res[0] = '0';
-		str_res[1] = '\0';
-	}
 	start = 0;
 	end = i - 1;
 	while (start < end)
@@ -70,5 +46,31 @@ char	*ft_get_charnum(int nb)
 		start++;
 		end--;
 	}
+}
+
+char	*ft_get_charnum(int nb)
+{
+	char			*str_res;
+	int				i;
+	unsigned int	ui;
+
+	i = 0;
+	str_res = malloc(get_digits(nb) + 1);
+	ui = nb;
+	while (ui > 0)
+	{
+		str_res[i++] = '0' + (ui % 10);
+		ui /= 10;
+	}
+	if (nb != 0)
+	{
+		str_res[i] = '\0';
+	}
+	else
+	{
+		str_res[0] = '0';
+		str_res[1] = '\0';
+	}
+	reverse(str_res, i);
 	return (str_res);
 }
